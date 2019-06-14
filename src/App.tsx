9868d3fb3e350ca75e9c2b5d9a2bc0c5c1b352cc
Soichiro-Yoshimura <yoshimura@soichiro.org>
2019-06-14 21:03:36 +0900

    react-bootstrapの廃止

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link} from "react-router-dom";
-import { Container, Navbar, Nav } from 'react-bootstrap';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 
 function App() {
   return (
-
     <Router>
       <div>
         <Header />
-        <Container>
+        <div className="container mt-3" >
           <Route exact path="/events" component={Events} />
           <Route path="/users" component={Users} />
           <Route path="/new" component={New} />
           <Route path="/topics" component={Topics} />
-        </Container>
+        </div>
       </div>
     </Router>
   );
@@ -76,15 +74,27 @@ function Topics({ match } : TopicsProps) {
 
 function Header() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand href="/">LT管理ツール</Navbar.Brand>
-      <Nav className="mr-auto">
-        <Nav.Link><Link to="/events">イベント一覧</Link></Nav.Link>
-        <Nav.Link><Link to="/users">ユーザー一覧</Link></Nav.Link>
-        <Nav.Link><Link to="/new">イベント作成</Link></Nav.Link>
-        <Nav.Link><Link to="/topics">Topics</Link></Nav.Link>
-      </Nav>
-    </Navbar>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <Link className="navbar-brand" to="/">LTマネージャー</Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/events" href="#">イベント一覧 </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/users" href="#">ユーザー一覧 </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/new" href="#">イベント作成</Link>
+          </li>
+        </ul>
+        <button className="btn btn-outline-light my-2 my-sm-0" type="submit">ログイン</button>
+      </div>
+    </nav>
   );
 }
 
